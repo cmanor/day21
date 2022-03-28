02f1482f906b1b9aba0d24bb37de5dac1eff8177
cmanor <101116379+cmanor@users.noreply.github.com>
2022-03-28 16:05:45 -0400

    actually finished

--- index.js
+++ index.js
@@ -9,34 +9,29 @@ class Job
     }
     getName()
     {
-    
+    return this.name;
     }
     getFieldAndIndustry()
     {
-    ;
+    return this.field + this.industry;
     }
 }
 
-class mcJob extends Job {
-    constructor(title, author, year, type) {
-        super(title, author, year);
-        this.type = type;
+class McJob extends Job {
+    constructor(name, industry, requiresDegree, field) 
+	{
+        super(name, industry, requiresDegree, field);
     }
 }
 
-let job1 = new Job(job1){
+let job1 = new Job("job1", "in1", false, "field1")
+let job2 = new Job("job2", "in2", false, "field2")
+let job3 = new Job("job3", "in3", false, "field3")
 
-}
-let job2 = new Job(job2){
-    
-}
-let job3 = new Job(job3){
-    
-}
+console.log(job1.getName() + job1.getFieldAndIndustry());
+console.log(job2.getName() + job2.getFieldAndIndustry());
+console.log(job3.getName() + job3.getFieldAndIndustry());
 
-console.log(job1.getName + job1.getFieldAndIndustry);
-console.log(job2.getName + job2.getFieldAndIndustry);
+let retail = new McJob("McJob1", "in4", false, "field4");
 
-let job4 = new mcJob {
-    
-}
+console.log(retail.getName() + retail.getFieldAndIndustry());
